fc1a833236b02fadc737a7c05dcb1c9b5bdb857e
nintzatekeen <09iker05@gmail.com>
2020-03-28 13:13:34 +0100

    Ignorar y vermoto

--- Vehiculos/src/Vehiculos/Motocicleta.java
+++ Vehiculos/src/Vehiculos/Motocicleta.java
@@ -34,6 +34,7 @@ public class Motocicleta extends Vehiculo{
 	}
 
 	public void verMoto (){
+		System.out.println("MOTO:");
 		System.out.println("La moto es de color "+getColor());
 		System.out.println("Tiene "+getNumRuedas()+" numero de ruedas");
 		System.out.println("Con una cilindrada de"+getCilindrada());
